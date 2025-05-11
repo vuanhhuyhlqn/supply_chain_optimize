ff6eb6d7c1a49865f113cb6fc8d1d6612a799811
madmoon <madmoontokyo@gmail.com>
2025-05-11 09:58:36 +0700

    import

--- indi.py
+++ indi.py
@@ -1,5 +1,10 @@
 import numpy as np
 from task import Task
+from plant import Plant
+from dc import DC
+from retailer import Retailer
+from customer import Customer
+from utils import dist
 
 class Individual:
     def __init__(self, task: Task, gene=None):
