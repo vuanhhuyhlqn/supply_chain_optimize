5edc4d88a2d8f5907ab6ffebd3ad65f27bd8dce2
madmoon <madmoontokyo@gmail.com>
2025-06-01 10:15:02 +0700

    stage

--- task_gen.py
+++ task_gen.py
@@ -1,7 +1,12 @@
 from task import Task
+from typing import List
 
 class TaskGenerator:
-    def __init__(self, num_task):
+    def __init__(self, num_tasks):
+        self.num_tasks = num_tasks
+    def compile(self, num_plants, num_dcs, num_retailers, num_customers):
         pass
     def gen(self):
-        pass
+        lst_tasks : List[Task] = []
+        for i in range(self.num_tasks):
+            pass
