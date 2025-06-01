a8a8274ba964741ae688e7e08726acce11382bdb
madmoon <madmoontokyo@gmail.com>
2025-06-01 10:46:50 +0700

    a mat & b mat

--- task.py
+++ task.py
@@ -19,7 +19,8 @@ class Task:
 		self.lst_retailers : List[Retailer] = []
 		self.lst_customers : List[Customer] = []
 		
-		self.coefficient_matrix = np.zeros(shape=(self.num_entities, self.num_entities))
+		self.a_mat = np.zeros(shape=(self.num_entities, self.num_entities, 4))
+		self.b_mat = np.zeros(shape=(self.num_entities, self.num_entities, 4))
 		
 	def add_plant(self, plant: Plant):
 		self.lst_plants.append(plant)
